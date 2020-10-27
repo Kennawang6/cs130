e931fe729af020cc1b6f8ec0a34d98631f6959ba
Kenna Wang <kenna.wang6@gmail.com>
2020-10-26 23:17:55 -0500

    Add callable functions

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -4,6 +4,24 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
+    // call with HTTP
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello, world!");
+});
+
+exports.authHelloWorld = functions.https.onCall((data, context) => {
+    // called with authentication from inside the app
+    if (!context.auth) {
+        functions.logger.info("Unauthenticated user");
+        return {text:"No."};
+    } else {
+        functions.logger.info("Hello to " + context.auth.uid);
+        return {text:"Hello, " + context.auth.uid + "!\n"};
+    }
+});
+
+exports.appHelloWorld = functions.https.onCall((data, context) => {
+    // called from inside the app (no auth needed)
+    functions.logger.info("Hello to app");
+    response.send("Hello, app");
 });

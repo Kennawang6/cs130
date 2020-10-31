a881da41e4d4afd2e41c2a3c4693c1ac1e61519e
Kenna Wang <kenna.wang6@gmail.com>
2020-10-31 17:22:17 -0500

    Add database accessor function
    This is just another sanity check function

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,8 +1,8 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
     // call with HTTP
   functions.logger.info("Hello logs!", {structuredData: true});
@@ -24,4 +24,19 @@ exports.appHelloWorld = functions.https.onCall((data, context) => {
     // called from inside the app (no auth needed)
     functions.logger.info("Hello to app");
     response.send("Hello, app");
+});
+
+exports.httpGet = functions.https.onRequest((request, response) => {
+  // need to add to local db emulator
+    db.collection('demo').doc('doc').get().then((doc) => {
+        if (doc.exists) {
+            functions.logger.info(doc.data()) + " from HTTP\n";
+            response.send(doc.data());
+        } else {
+            functions.logger.info("document not found\n");
+            response.send("Something went wrong\n");
+        }
+    }).catch((error) => {
+        functions.logger.info("database get error " + error);
+    });
 });

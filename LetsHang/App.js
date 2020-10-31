be90cf563486cd13e9a4c823f61af428290b56c6
EmilyLiu18 <tong9718@gmail.com>
2020-10-30 22:10:12 -0700

    Google Signed In DONE

--- LetsHang/App.js
+++ LetsHang/App.js
@@ -1,22 +1,38 @@
 /**
- * Sample React Native App with Firebase
- * https://github.com/invertase/react-native-firebase
+ * Sample React Native App
+ * https://github.com/facebook/react-native
  *
  * @format
- * @flow
+ * @flow strict-local
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import auth from '@react-native-firebase/auth';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 import firebase from '@react-native-firebase/app';
+import { Button } from 'react-native';
 
-// TODO(you): import any additional firebase services that you require for your app, e.g for auth:
-//    1) install the npm package: `yarn add @react-native-firebase/auth@alpha` - you do not need to
-//       run linking commands - this happens automatically at build time now
-//    2) rebuild your app via `yarn run run:android` or `yarn run run:ios`
-//    3) import the package here in your JavaScript code: `import '@react-native-firebase/auth';`
-//    4) The Firebase Auth service is now available to use here: `firebase.auth().currentUser`
+import { GoogleSignin } from '@react-native-community/google-signin';
+
+GoogleSignin.configure({
+  webClientId: '788957977133-11kf53kgqlla41v5r5sgagj09pn4u9vd.apps.googleusercontent.com',
+});
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
@@ -30,18 +46,84 @@ const firebaseCredentials = Platform.select({
 
 type Props = {};
 
+function LoginApp() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <Text>Welcome {user.email}</Text>
+    </View>
+  );
+}
+
+
 export default class App extends Component<Props> {
+
+  onGoogleButtonPress = async() => {
+    /*var provider = new firebase.auth.GoogleAuthProvider();
+    provider.setCustomParameters({
+      prompt: 'select_account'
+    });
+    return firebase.auth().signInWithPopup(provider);
+*/
+
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);  
+  }
+
+  logoff = () => {
+    auth()
+    .signOut()
+    .then(() => console.log('User signed out!'));
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native + Firebase!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <LoginApp />
+        
         {!firebase.apps.length && (
           <Text style={styles.instructions}>
             {`\nYou currently have no Firebase apps registered, this most likely means you've not downloaded your project credentials. Visit the link below to learn more. \n\n ${firebaseCredentials}`}
           </Text>
         )}
+
+        <Button
+          title="Google Sign-In"
+          onPress={() => this.onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+        />
+        <Button
+          title="Log Off"
+          onPress={this.logoff}
+        />
       </View>
     );
   }

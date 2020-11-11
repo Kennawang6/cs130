ae16df6ed1d0d2db5d0ec75615e0b74104df645f
Kenna Wang <kenna.wang6@gmail.com>
2020-11-10 20:08:57 -0600

    Clean schedule.js somewhat

--- backend/functions/schedule.js
+++ backend/functions/schedule.js
@@ -11,7 +11,7 @@ class Schedule {
 var scheduleConverter = {
   toFirestore: function(schedule) {
     return {
-      timeslots: schedule.timeslots;
+      timeslots: schedule.timeslots
     }
   },
   fromFirestore: function(snapshot, options) {
@@ -22,35 +22,39 @@ var scheduleConverter = {
 
 exports.addSchedule = functions.https.onCall((data, context) => {
   if (!context.auth) {
-    return {text: "Cannot save schedule for user who is not logged in"};
+    functions.logger.info("Unauthenticated user");
+    return {status: "not ok", text: "Unauthenticated user"};
   } else {
     const id = context.auth.uid;
     functions.logger.info("saving schedule of user with id " + id + "\n");
-    db.collection('schedules').doc(id).withConverter(scheduleConverter).set(new Schedule(data)).then({}
-      functions.logger.info("saved schedule\n");
+
+    db.collection('schedules').doc(id).withConverter(scheduleConverter).set(new Schedule(data)).then(() => {
+      functions.logger.info("Saved schedule for user " + id + "\n");
       return {status: "ok"};
     }).catch((error) => {
       functions.logger.info("could not save schedule of user with id " + id + ", error: " + error);
-      return {status: "not ok", message: error};
+      return {status: "not ok", text: error};
     });
   }
 });
 
 exports.getSchedule = functions.https.onCall((data, context) => {
   if (!context.auth) {
-    return {text: "Cannot retrieve schedule for user who is not logged in"};
+    functions.logger.info("Unauthenticated user");
+    return {status: "not ok", text: "Unauthenticated user"};
   } else {
     const id = context.auth.uid
-    functions.logger.info("getting schedule for user with id " + id + "\n");
+    functions.logger.info("Getting schedule for user with id " + id + "\n");
+
     db.collection('schedules').doc(id).withConverter(scheduleConverter).get().then((doc) => {
       if (doc.exists) {
         return {status: "ok", schedule: doc.data()};
       } else {
-        return {status: "not ok", message: "no schedule found for user"}
+        return {status: "not ok", text: "No schedule found for user"}
       }
     }).catch((error) => {
-      functions.logger.info("could not get schedule for user with id " + id + ", error: " + error + "\n");
-      return {status: "not ok", message: error};
+      functions.logger.info("Could not get schedule for user with id " + id + ", error: " + error + "\n");
+      return {status: "not ok", text: error};
     });
   }
 });

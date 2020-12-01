b6d132360eefb232efdd876fed242973db546498
Yu-Hsuan Liu <44049122+janny5005@users.noreply.github.com>
2020-12-02 04:08:40 +0800

    addSchedule done

--- LetsHang/components/schedule/addSchedule.js
+++ LetsHang/components/schedule/addSchedule.js
@@ -26,13 +26,17 @@ const addSchedule = props => {
   // state
   const [description, setDescription] = useState('');
 
-  //initialize
+  //initialize variable
   const monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   var timeZone = 0;
-  var inputFormat = false;
+  var EventList = [];
+  var inputInvalidFormat = true;
+  var inputOverlap = true;
+  var eventConflict = true;
   var timeslot = {'start':0, 'end':0, 'id':0, 'description':''}
+  var timeslots = [];
 
-
+  // snapshot to get user's timezone
   var subscribe = firestore().collection("users").onSnapshot(snapshot => {
         var curUser = firebase.auth().currentUser.uid;
         snapshot.forEach(doc => {
@@ -40,12 +44,14 @@ const addSchedule = props => {
             var curID = doc.ref._documentPath._parts[1];
             if (curID==curUser){
                 timeZone = userInfo.timeZone;
+                EventList = userInfo.events;
             }
         });
         console.log('timeZone');
         console.log(timeZone);
   });
 
+  // useInput for dateTimePucker
   function useInput() {
       const [date, setDate] = useState(new Date());
       const [mode, setMode] = useState('date');
@@ -81,91 +87,111 @@ const addSchedule = props => {
   const start = useInput(new Date())
   const end = useInput(new Date())
 
-  const getUserTimeZone = async() => {
-    const data = await functions().httpsCallable('getUserData')({});
-    timeZone = data.data.data.timeZone;
-    if(timeZone<-12||timeZone>12){
-      timeZone = 0;
-    }
-  }
-
+  // firebase: addTimeslotToSchedule
   const sendScheduleEvent = async() => {
-       // might need to loop thorough timeslots
       const data = await functions().httpsCallable('addTimeslotToSchedule')({
               timeslot: timeslot
       });
       console.log("addTimeslotToSchedule function has been called");
       console.log(data);
-      //alert('Event added to schedule.');
   }
 
   handlePress = () => {
       console.log("Button was pressed");
       console.log(description);
       timeslot.description = description;
-      getUserTimeZone();
-      InputValidation(start.date,end.date);
-      //TODO: might need to loop through timeslots to check if it is overlapped
 
-      dispatch({
-        type: ADD_SCHEDULE,
-        start: timeslot.start,
-        end: timeslot.end,
-        description: timeslot.description,
-        id: timeslot.id
-      });
-      console.log("Dispatched to store: " + JSON.stringify(schedule));
-      // Add timeslots to firebase
-      sendScheduleEvent();
-      // Back to Schedule
-      props.navigation.navigate('Schedule');
+      InputValidation(start.date,end.date);
+      CheckOverlap();
+      if(inputInvalidFormat||inputOverlap){
+          console.log('Not added.')
+      }else{
+        //update redux
+        dispatch({
+          type: ADD_SCHEDULE,
+          start: timeslot.start,
+          end: timeslot.end,
+          description: timeslot.description,
+          id: timeslot.id
+        });
+        console.log("Dispatched to store: " + JSON.stringify(schedule.scheduledEvents));
+
+        // Add timeslots to firebase
+        sendScheduleEvent();
+
+        //Update Event Schedule
+        UpdateEvents();
+
+        // Back to Schedule
+        props.navigation.navigate('Schedule');
+      }
 
   }
 
   InputValidation = (s,e) => {
       console.log('Input Validation');
-      console.log(s.toString());
-      console.log(e.toString());
-      //TODO:Check START and END time
-      //Check if start < end, otherwise, alert
-      if (s.getTime() >= e.getTime()){
-         alert('End time should be larger than start time.');
-         return;
-      }
-      //Set seconds to 0 for start and end
-      s.setSeconds(0);
-      e.setSeconds(0);
-      //TODO:Set Time Zone
-      // 'July 20, 69 00:20:18 UTC+07:00'
-      // - access user time zone from firebase
-      // - event.toString() expected output format: Tue Aug 19 1975 23:15:30 GMT+0200 (CEST)
-      //get UserTimeZone
-      subscribe();
-      if (timeZone>=10){
-        var start_string = monthName[s.getMonth()] + ' ' + s.getDate() + ', ' + s.getFullYear() + ' ' + s.getHours() + ':' + s.getMinutes() + ':00 UTC+' + timeZone + ':00';
-        var end_string = monthName[e.getMonth()] + ' ' + e.getDate() + ', ' + e.getFullYear() + ' ' + e.getHours() + ':' + e.getMinutes() + ':00 UTC+' + timeZone + ':00';
-      }else if (timeZone>=0){
-        var start_string = monthName[s.getMonth()] + ' ' + s.getDate() + ', ' + s.getFullYear() + ' ' + s.getHours() + ':' + s.getMinutes() + ':00 UTC+0' + timeZone + ':00';
-        var end_string = monthName[e.getMonth()] + ' ' + e.getDate() + ', ' + e.getFullYear() + ' ' + e.getHours() + ':' + e.getMinutes() + ':00 UTC+0' + timeZone + ':00';
-      }else if (timeZone > -10){
-        var start_string = monthName[s.getMonth()] + ' ' + s.getDate() + ', ' + s.getFullYear() + ' ' + s.getHours() + ':' + s.getMinutes() + ':00 UTC-0' + Math.abs(timeZone) + ':00';
-        var end_string = monthName[e.getMonth()] + ' ' + e.getDate() + ', ' + e.getFullYear() + ' ' + e.getHours() + ':' + e.getMinutes() + ':00 UTC-0' + Math.abs(timeZone) + ':00';
+      //Check description not empty
+      if(description == ""){
+        alert('Description should not be empty.');
       }else{
-        var start_string = monthName[s.getMonth()] + ' ' + s.getDate() + ', ' + s.getFullYear() + ' ' + s.getHours() + ':' + s.getMinutes() + ':00 UTC' + timeZone + ':00';
-        var end_string = monthName[e.getMonth()] + ' ' + e.getDate() + ', ' + e.getFullYear() + ' ' + e.getHours() + ':' + e.getMinutes() + ':00 UTC' + timeZone + ':00';
+          console.log(s.toString());
+          console.log(e.toString());
+          //Check START and END time
+          // - Check if start < end, otherwise, alert
+          if (s.getTime() >= e.getTime()){ //
+             alert('End time should be larger than start time.');
+          }else{
+              //Set seconds to 0 for start and end
+              s.setSeconds(0);
+              e.setSeconds(0);
+              //Set Time Zone
+              // - format: 'July 20, 69 00:20:18 UTC+07:00'
+              // - access user time zone from firebase
+              subscribe(); // get UserTimeZone
+              if (timeZone>=10){
+                var start_string = monthName[s.getMonth()] + ' ' + s.getDate() + ', ' + s.getFullYear() + ' ' + s.getHours() + ':' + s.getMinutes() + ':00 UTC+' + timeZone + ':00';
+                var end_string = monthName[e.getMonth()] + ' ' + e.getDate() + ', ' + e.getFullYear() + ' ' + e.getHours() + ':' + e.getMinutes() + ':00 UTC+' + timeZone + ':00';
+              }else if (timeZone>=0){
+                var start_string = monthName[s.getMonth()] + ' ' + s.getDate() + ', ' + s.getFullYear() + ' ' + s.getHours() + ':' + s.getMinutes() + ':00 UTC+0' + timeZone + ':00';
+                var end_string = monthName[e.getMonth()] + ' ' + e.getDate() + ', ' + e.getFullYear() + ' ' + e.getHours() + ':' + e.getMinutes() + ':00 UTC+0' + timeZone + ':00';
+              }else if (timeZone > -10){
+                var start_string = monthName[s.getMonth()] + ' ' + s.getDate() + ', ' + s.getFullYear() + ' ' + s.getHours() + ':' + s.getMinutes() + ':00 UTC-0' + Math.abs(timeZone) + ':00';
+                var end_string = monthName[e.getMonth()] + ' ' + e.getDate() + ', ' + e.getFullYear() + ' ' + e.getHours() + ':' + e.getMinutes() + ':00 UTC-0' + Math.abs(timeZone) + ':00';
+              }else{
+                var start_string = monthName[s.getMonth()] + ' ' + s.getDate() + ', ' + s.getFullYear() + ' ' + s.getHours() + ':' + s.getMinutes() + ':00 UTC' + timeZone + ':00';
+                var end_string = monthName[e.getMonth()] + ' ' + e.getDate() + ', ' + e.getFullYear() + ' ' + e.getHours() + ':' + e.getMinutes() + ':00 UTC' + timeZone + ':00';
+              }
+              console.log(start_string)
+              console.log(end_string)
+              var temp_s = new Date(start_string);
+              var temp_e = new Date(end_string);
+              timeslot.start = temp_s.getTime();
+              timeslot.id = temp_s.getTime();
+              timeslot.end = temp_e.getTime();
+              console.log(timeslot);
+              inputInvalidFormat = false;
+          }
       }
-      console.log(start_string)
-      console.log(end_string)
-      var temp_s = new Date(start_string);
-      var temp_e = new Date(end_string);
-      timeslot.start = temp_s.getTime();
-      timeslot.id = temp_s.getTime();
-      timeslot.end = temp_e.getTime();
-      console.log(timeslot);
-      inputFormat = true;
   }
 
-  //TODO: Coordinate with Event
+  CheckOverlap = () => {
+    console.log('CheckOverlap')
+    console.log(schedule.scheduledEvents)
+    var size = schedule.scheduledEvents.length;
+    inputOverlap = false;
+    //console.log(size);
+    for(var i =0; i < size;i++){
+        existedTimeslot = schedule.scheduledEvents[i];
+        console.log(existedTimeslot)
+        if(timeslot.start > existedTimeslot.end || timeslot.end < existedTimeslot.start){
+            console.log(i+': not overlapped')
+        }else{
+            inputOverlap = true;
+            alert('This schedule overlap with other schedule.');
+        }
+    }
+  }
+  //Coordinate with Event
   // need to consider various cases
   // access user event list
   // go through all the event
@@ -175,7 +201,32 @@ const addSchedule = props => {
   //        event finalize
   //        event update
   //
+  UpdateEvents = () =>{
+    for(var i =0; i < EventList.length ;i++){
+        var eventID = EventList[i];
+        UpdateUserSchedule(eventID);
+        //getEvent(eventID)
+        //check time conflict
+        //addUserScheduleToEvent(eventID)
+        //computedNextEarliestAvailableTime
+    }
+  }
 
+  const UpdateUserSchedule = async(eventID) => {
+    const eventInfo = await functions().httpsCallable('getEvent')({event_id: eventID});
+    console.log("getEvent function has been called");
+    console.log(eventInfo);
+    const data1 = await functions().httpsCallable('addUserScheduleToEvent')({event_id: eventID});
+    console.log("addUserScheduleToEvent function has been called");
+    const data2  = await functions().httpsCallable('computedNextEarliestAvailableTime')({event_id: eventID});
+    console.log("computedNextEarliestAvailableTime function has been called");
+    const newEventInfo = await functions().httpsCallable('getEvent')({event_id: eventID});
+    console.log("getEvent function has been called");
+    console.log(newEventInfo);
+  }
+
+
+    // display format for datetimepicker
     var startDate = start.date.toDateString();
     var s_minutes = ("0" + start.date.getMinutes()).slice(-2);
     var startTime = start.date.getHours() + ":" + s_minutes;
--- LetsHang/components/schedule/schedule.js
+++ LetsHang/components/schedule/schedule.js
@@ -30,21 +30,23 @@ class Schedule extends Component{
           timeZoneLoading: true,
           timeZone: null
         };
-
         this.subscriber = firestore().collection('schedules').onSnapshot(snapshot => {
             console.log('snapshot');
            // console.log(snapshot);
             var curUser = firebase.auth().currentUser.uid;
             //console.log('curUser');
             //console.log(curUser);
+
+            var existInFirebase = false;
             snapshot.forEach(doc => {
                 var curTimeslots = doc.data();
-                //console.log('curTimeslots')
-                //console.log(curTimeslots.timeslots);
+                console.log('curTimeslots')
+                console.log(curTimeslots.timeslots);
                 var curID = doc.ref._documentPath._parts[1];
                 //console.log('curID');
                 //console.log(curID);
                 if (curID==curUser){
+                    existInFirebase = true;
                     this.props.replaceSchedule(curTimeslots.timeslots);
                     console.log('this.props.scheduledEvents')
                     console.log(this.props.scheduledEvents)
@@ -58,16 +60,19 @@ class Schedule extends Component{
                     });
                 }
             });
+
+            if(!existInFirebase){
+                this.getScheduleData();
+            }
         });
     }
-    /*
-    componentDidMount() {
-      this.getScheduleData();
-    }
-    */
+
+    // access user timezone
     componentDidMount() {
         this.getUserTimeZone();
     }
+
+    // access user timezone
     getUserTimeZone = async() => {
         const data = await functions().httpsCallable('getUserData')({});
         var timeZone = data.data.data.timeZone;
@@ -78,15 +83,16 @@ class Schedule extends Component{
      }
 
      //TODO: Double check this part
+
     componentDidUpdate(prevProps, prevState) {
       // only update chart if the data has changed
-      if(prevProps.scheduledEvents !== this.props.scheduledEvents){
+      if(prevProps.scheduleEvents !== this.props.scheduleEvents){
         this.setState({ifLoading:true});
         console.log(this.state.ifLoading);
         this.getScheduleData();
       }
     }
-/*
+    /*
     componentDidUpdate(prevProps) {
         //console.log('Should I update?');
         //console.log("new friends length: ", this.props.friends.length);
@@ -123,17 +129,27 @@ class Schedule extends Component{
             displaySchedule: this.state.displaySchedule
           }
         })
-        this.test();
+        //this.test();
     }
 
-    //
+    /*
+    //test redux
     test = () =>{
         console.log("Try redux");
         console.log(this.props.scheduledEvents);
-    }
+    }*/
+
 
     render() {
       //console.log('in render');
+      //TODO: Handle Split Events here
+
+      const displayEvents = [];
+      for(var i=0;i++;i<this.props.scheduledEvents.length){
+
+
+      }
+
 
       if(this.state.ifLoading||this.state.timeZoneLoading){
         return(
@@ -145,39 +161,38 @@ class Schedule extends Component{
             />
             </View>
         );
-      }
-      else{
+      }else{
       return (
         <View>
           <WeeklyCalendar
             events= {this.props.scheduledEvents}
             renderEvent={(event, j) => {
-              console.log('in render Event')
+              //console.log('in render Event')
               var s = new Date(event.start);
-              console.log('s')
-              console.log(s.toString())
+              //console.log('s')
+              //console.log(s.toString())
               var s_minutes = ("0" + s.getMinutes()).slice(-2);
               var e = new Date(event.end);
-              console.log('e')
-              console.log(e.toString())
+              //console.log('e')
+              //console.log(e.toString())
               var e_minutes = ("0" + e.getMinutes()).slice(-2);
-              console.log(this.state.timeZone);
+              //console.log(this.state.timeZone);
               var s_hours = s.getUTCHours() + this.state.timeZone;
-              console.log(s_hours);
+              //console.log(s_hours);
               if(s.getHours()<12){
                 var startTime = s.getHours() + ":" + s_minutes + ' AM';
               }else{
-                var startTime = s.getHours() + ":" + s_minutes + ' PM';
+                var startTime = s.getHours()-12 + ":" + s_minutes + ' PM';
               }
               if(e.getHours()<12){
                 var endTime = e.getHours() + ":" + e_minutes + ' AM';
               }else{
-                var endTime = e.getHours() + ":" + e_minutes + ' PM';
+                var endTime = e.getHours()-12 + ":" + e_minutes + ' PM';
               }
               //console.log(start_string);
-              console.log(startTime);
+              //console.log(startTime);
               //console.log(end_string);
-              console.log(endTime);
+              //console.log(endTime);
 
               return (
                 <View key={j}>
@@ -203,32 +218,32 @@ class Schedule extends Component{
               )
             }}
             renderLastEvent={(event, j) => {
-              console.log('in render lastEvent')
+              //console.log('in render lastEvent')
               var s = new Date(event.start);
-              console.log('s')
-              console.log(s.toString())
+              //console.log('s')
+              //console.log(s.toString())
               var s_minutes = ("0" + s.getMinutes()).slice(-2);
               var e = new Date(event.end);
-              console.log('e')
-              console.log(e.toString())
+              //console.log('e')
+              //console.log(e.toString())
               var e_minutes = ("0" + e.getMinutes()).slice(-2);
-              console.log(this.state.timeZone);
+              //console.log(this.state.timeZone);
               var s_hours = s.getUTCHours() + this.state.timeZone;
-              console.log(s_hours);
+              //console.log(s_hours);
               if(s.getHours()<12){
                 var startTime = s.getHours() + ":" + s_minutes + ' AM';
               }else{
-                var startTime = s.getHours() + ":" + s_minutes + ' PM';
+                var startTime = s.getHours()-12 + ":" + s_minutes + ' PM';
               }
               if(e.getHours()<12){
                 var endTime = e.getHours() + ":" + e_minutes + ' AM';
               }else{
-                var endTime = e.getHours() + ":" + e_minutes + ' PM';
+                var endTime = e.getHours()-12 + ":" + e_minutes + ' PM';
               }
               //console.log(start_string);
-              console.log(startTime);
+              //console.log(startTime);
               //console.log(end_string);
-              console.log(endTime);
+              //console.log(endTime);
 
               return (
                 <View key={j}>
